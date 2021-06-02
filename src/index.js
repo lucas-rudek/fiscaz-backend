7533cab1b7a5898336ce0d9b640ccdf4be2030b3
Lucas Rudek <lucas-rudek@hotmail.com>
2021-06-02 12:16:34 -0300

    changes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-var http = require("http");
+const express = require("express");
+const app = express();
 
-//create a server object:
-http
-  .createServer(function (req, res) {
-    res.write("Hello World!!!!!"); //write a response to the client
-    res.end(); //end the response
-  })
-  .listen(8080); //the server object listens on port 8080
+app.listen(8080, function (req, res) {
+  console.log("listening on 8080");
+  app.get("/", function (req, res) {
+    res.send("Hello World!!!");
+  });
+});
