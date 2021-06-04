d6459724b7e2b8371644d704e8dab1ae7eb9b09e
Lucas Rudek <lucas-rudek@hotmail.com>
2021-06-04 16:55:16 -0300

    04-06

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
-app.listen(8080, function (req, res) {
-  console.log("listening on 8080");
-  app.get("/", function (req, res) {
-    res.send("Hello World!!!");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(8080, (req, res) => {
+  app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+  });
+  app.post("/aaaa", (req, res) => {
+    console.log(req.body);
   });
 });
