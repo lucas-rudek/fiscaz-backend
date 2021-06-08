ba9cc581bdcdf4c4fac49a67af92c5727cf96b07
Lucas Rudek <lucas-rudek@hotmail.com>
2021-06-08 16:41:13 -0300

    Update db.config.js

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   url:
-    "mongodb+srv://lucas:Moitaazul88@cluster0.xguoi.mongodb.net/fiscaz?retryWrites=true&w=majority"
+    "mongodb+srv://lucas:@cluster0.xguoi.mongodb.net/fiscaz?retryWrites=true&w=majority"
 };
