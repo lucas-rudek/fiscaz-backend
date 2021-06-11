ee07872ca026d0d2077987a61cc31d732700459c
Lucas Rudek <lucas-rudek@hotmail.com>
2021-06-11 12:24:08 -0300

    refactoring

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,45 +1,43 @@
 var express = require("express");
 var router = express.Router();
-var path = require("path");
 
-const TestController = require("../controller/TestController.js");
+const Controller = require("../controller/Processos_Controller.js");
 
 router.get("/", async (req, res) => {
-  //res.sendFile(path.join(__dirname, "../view", "index.html"));
   res.send("Hello");
 });
 
-router.get("/usuarios", async (req, res) => {
-  const c = await TestController.findAll_teste();
+router.get("/processos", async (req, res) => {
+  const c = await Controller.findAll_teste();
   res.json(c);
 });
 
-router.post("/usuarios", async (req, res) => {
-  await TestController.create_teste({
+router.post("/processos", async (req, res) => {
+  await Controller.create_teste({
     name: req.body.name,
     quote: req.body.quote
   });
-  res.send(`Usuário criado: ${req.body.name}.`);
+  res.send(`Processo criado: ${req.body.name}.`);
 });
 
-router.get("/usuarios/:name", async (req, res) => {
-  const c = await TestController.find_teste(req.params.name);
+router.get("/processos/:name", async (req, res) => {
+  const c = await Controller.find_teste(req.params.name);
   res.json(c);
 });
 
-router.post("/usuarios/delete/:name", async (req, res) => {
-  await TestController.delete_teste({
+router.post("/processos/delete/:name", async (req, res) => {
+  await Controller.delete_teste({
     name: req.body.name
   });
-  res.send(`Usuário deletado: ${req.body.name}.`);
+  res.send(`Processo deletado: ${req.body.name}.`);
 });
 
-router.post("/usuarios/update/:name", async (req, res) => {
-  await TestController.update_teste({
+router.post("/processos/update/:name", async (req, res) => {
+  await Controller.update_teste({
     name: req.body.name,
     quote: req.body.quote
   });
-  res.send(`Usuário atualizado: ${req.body.name}.`);
+  res.send(`Processo atualizado: ${req.body.name}.`);
 });
 
 module.exports = router;
