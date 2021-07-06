ade43b1fa07c054840f9f311c67157d125e9985c
Lucas Rudek <lucas-rudek@hotmail.com>
2021-07-05 21:12:25 -0300

    s3 config

--- src/server.js
+++ src/server.js
@@ -2,9 +2,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const AWS = require("aws-sdk");
+const uuid = require("uuid");
 const app = express();
 const dbConfig = require("./config/db.config.js");
 const router = require("./routes/router.js");
+
+process.env.AWS_ACCESS_KEY_ID = "AKIASCODF2CXOFWPCO5V";
+process.env.AWS_SECRET_ACCESS_KEY = "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";
 
 const corsOptions = {
   origin: "*" //fiscaz front
@@ -29,3 +34,36 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
+
+// Create unique bucket name
+var bucketName = "node-sdk-sample-" + uuid.v4();
+// Create name for uploaded object key
+var keyName = "hello_world.txt";
+
+// Create a promise on S3 service object
+var bucketPromise = new AWS.S3({ apiVersion: "2006-03-01" })
+  .createBucket({ Bucket: bucketName })
+  .promise();
+
+// Handle promise fulfilled/rejected states
+bucketPromise
+  .then(function (data) {
+    // Create params for putObject call
+    var objectParams = {
+      Bucket: bucketName,
+      Key: keyName,
+      Body: "Hello World!"
+    };
+    // Create object upload promise
+    var uploadPromise = new AWS.S3({ apiVersion: "2006-03-01" })
+      .putObject(objectParams)
+      .promise();
+    uploadPromise.then(function (data) {
+      console.log(
+        "Successfully uploaded data to " + bucketName + "/" + keyName
+      );
+    });
+  })
+  .catch(function (err) {
+    console.error(err, err.stack);
+  });
